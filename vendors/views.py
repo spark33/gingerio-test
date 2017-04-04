52958248add1f6d826446e35edde5412f0f46a4f
Sean Park <spark3@andrew.cmu.edu>
2017-04-04 02:24:02 -0400

    added pagination

--- vendors/views.py
+++ vendors/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Vendor
 import datetime, operator
@@ -12,12 +13,21 @@ def index(request):
 	recent_event_counts = map((lambda v: v.events.filter(start_time__gte=last_month).count()), Vendor.objects.all())
 	vendor_list = list(zip(vendors, recent_event_counts))
 	vendor_list.sort(key=operator.itemgetter(1), reverse=True)
-	print(vendor_list)
 	template = loader.get_template('vendors/index.html')
-	context = {
-		'vendor_list': vendor_list
-	}
-	return render(request, 'vendors/index.html', context)
+
+	paginator = Paginator(vendor_list, 10) # Show 10 vendors per page
+
+	page = request.GET.get('page')
+	try:
+		vendors = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		vendors = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		vendors = paginator.page(paginator.num_pages)
+
+	return render(request, 'vendors/index.html', {'vendors': vendors})
   
 
 def detail(request, vendor_id):
